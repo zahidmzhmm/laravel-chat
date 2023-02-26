6b1821a541304a3a705d88c5a5e6145b9000ec95
Zahid Mzhmm <zahidmzhmm@gmail.com>
2023-02-26 12:15:22 +0600

    Some Changes

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -7,37 +7,31 @@ use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class MessageSent implements ShouldBroadcast
+class MessageSent implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    public $sender_id;
-    public $rec_id;
     public $message;
 
     /**
      * Create a new event instance.
      */
-    public function __construct($sender_id, $rec_id, $msg)
+    public function __construct($message)
     {
-        $this->sender_id = $sender_id;
-        $this->rec_id = $rec_id;
-        $this->message = $msg;
+        $this->message = $message;
     }
 
     /**
      * Get the channels the event should broadcast on.
      *
-     * @return array<int, \Illuminate\Broadcasting\Channel>
+     * @return Channel
      */
-    public function broadcastOn(): array
+    public function broadcastOn()
     {
-        return [
-            new PrivateChannel('chat-' . $this->sender_id),
-            new PrivateChannel('chat-' . $this->rec_id)
-        ];
+        return new Channel('Test');
     }
 }
--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,10 +36,11 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'host' => env('PUSHER_HOST') ?: 'api-' . env('PUSHER_APP_CLUSTER', 'mt1') . '.pusher.com',
+                'host' => env('PUSHER_HOST', '127.0.0.1'),
+//                'host' => env('PUSHER_HOST') ?: 'api-' . env('PUSHER_APP_CLUSTER', 'mt1') . '.pusher.com',
                 'port' => env('PUSHER_PORT', 443),
                 'scheme' => env('PUSHER_SCHEME', 'https'),
-                'encrypted' => true,
+                'encrypted' => env('PUSHER_ENCRYPT', true),
                 'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
             ],
             'client_options' => [
