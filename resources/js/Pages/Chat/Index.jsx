582c0a875fafef86dd296a54289d807f43198b2b
Zahid Mzhmm <zahidmzhmm@gmail.com>
2023-02-26 13:23:41 +0600

    Design Added

--- resources/js/Pages/Chat/Index.jsx
+++ resources/js/Pages/Chat/Index.jsx
@@ -1,21 +1,42 @@
-import React from 'react';
-import {Head} from "@inertiajs/react";
+import ChatInput from '@/Components/Chat/ChatInput';
+import ChatMessages from '@/Components/Chat/ChatMessages';
+import ChatSidebar from '@/Components/Chat/ChatSidebar';
+import ChatUserInfoHeader from '@/Components/Chat/ChatUserInfoHeader';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 
-const Index = ({auth, mustVerifyEmail, status}) => {
+export default function Index(props) {
+    console.log(props);
     return (
-        <>
+        <AuthenticatedLayout
+            auth={props.auth}
+            errors={props.errors}
+        >
+            <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div className="messanger overflow-hidden p-4" style={{height: "92vh"}}>
+                    <div className="flex">
+                        <div className="basis-2/6 border-r border-slate-100 bg-white pt-3">
+                            {/*<ChatSidebar recentMessages={props.recentMessages}/>*/}
+                        </div>
 
-            <AuthenticatedLayout auth={auth}>
-                <Head title="Chat"/>
-                <div className="py-12">
-                    <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-                        This is User Chatting Section
+                        <div className="basis-4/6">
+                            <div className="flex justify-center items-center bg-slate-100 h-screen">
+                                <p className="font-bold text-3xl text-gray-500">
+                                    Please select a User to start chatting...
+                                </p>
+                            </div>
+                            {/* <ChatUserInfoHeader />
+
+                            <div className="messanger mt-4">
+                                <div className="px-4">
+                                    <ChatMessages />
+                                </div>
+
+                                <ChatInput />
+                            </div> */}
+                        </div>
                     </div>
                 </div>
-            </AuthenticatedLayout>
-        </>
+            </div>
+        </AuthenticatedLayout>
     );
-};
-
-export default Index;
+}
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,16 +1,16 @@
 import './bootstrap';
 import '../css/app.css';
 
-import { createRoot } from 'react-dom/client';
-import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import {createRoot} from 'react-dom/client';
+import {createInertiaApp} from '@inertiajs/react';
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
-    setup({ el, App, props }) {
+    setup({el, App, props}) {
         const root = createRoot(el);
 
         root.render(<App {...props} />);
@@ -19,3 +19,7 @@ createInertiaApp({
         color: '#4B5563',
     },
 });
+Echo.channel(`messenger`)
+    .listen('MessageSent', (e) => {
+        console.log(e.message);
+    });
