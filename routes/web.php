ef463accc02e4e6146740ae258090cdb66cf49ee
Zahid Mzhmm <zahidmzhmm@gmail.com>
2023-02-26 11:28:48 +0600

    Some Changes

--- routes/web.php
+++ routes/web.php
@@ -33,10 +33,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('chats', [\App\Http\Controllers\ChatController::class, 'index'])->name('chats.index');
-    Route::get('users', [\App\Http\Controllers\ChatController::class, 'users'])->name('users');
-    Route::get('chats/messages', [\App\Http\Controllers\ChatController::class, 'messages'])->name('chats.messages');
-    Route::post('chats/messages', [\App\Http\Controllers\ChatController::class, 'sendMessage'])->name('chats.send.message');
 });
 
 require __DIR__ . '/auth.php';
